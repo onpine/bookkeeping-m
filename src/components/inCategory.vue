fc6f3a68cb576884ed445c1eaf769e36850890d4
onpine <718215627@qq.com>
2021-04-08 19:50:26 +0800

    页面优化

--- src/components/inCategory.vue
+++ src/components/inCategory.vue
@@ -35,16 +35,19 @@
         text="设置"
       />
     </van-grid>
-    <van-field
-      v-model="message"
-      rows="2"
-      autosize
-      label="备注"
-      type="textarea"
-      maxlength="50"
-      placeholder="备注"
-      show-word-limit
-    />
+
+    <van-row>
+      <van-col :span="12">
+        <van-button type="default" @click="textShow = true">{{
+          message ? message : "添加备注"
+        }}</van-button>
+      </van-col>
+      <van-col :span="12">
+        <van-button type="default" @click="timeShow = true">{{
+          time
+        }}</van-button>
+      </van-col>
+    </van-row>
     <van-number-keyboard
       :show="inshow"
       v-model="value"
@@ -53,11 +56,54 @@
       close-button-text="完成"
       @blur="show = false"
     />
+    <van-popup
+      v-model="textShow"
+      round
+      :style="{ width: '90%', padding: '5%' }"
+    >
+      <span class="textLabel">填写备注：</span>
+      <van-field
+        v-model="message2"
+        rows="2"
+        autosize
+        border
+        type="textarea"
+        maxlength="50"
+        placeholder="备注"
+        show-word-limit
+      />
+      <van-row>
+        <van-col span="10" offset="1">
+          <van-button round plain type="primary" @click="textShow = false"
+            >取消</van-button
+          >
+        </van-col>
+        <van-col span="10" offset="2"
+          ><van-button round type="primary" @click="handleOk">确认</van-button>
+        </van-col>
+      </van-row>
+    </van-popup>
+    <van-popup
+      v-model="timeShow"
+      round
+      :style="{ width: '90%', padding: '5%' }"
+    >
+      <van-datetime-picker
+        v-model="currentDate"
+        type="datetime"
+        title="选择完整时间"
+        :min-date="minDate"
+        :max-date="maxDate"
+        @confirm="handleTime"
+        @cancel="handleCancel"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 import { inCategory } from "@/utils/Category.json";
+import { getFormat1 } from "@/utils/time.ts";
 export default {
   name: "inCategoryContainer",
   components: {},
@@ -65,10 +111,17 @@ export default {
   data() {
     return {
       inCategory: inCategory,
-      inshow: false,
+      inshow: true,
       value: "",
       message: "",
+      message2: "",
+      time: getFormat1(new Date()),
       selected: 0,
+      textShow: false,
+      timeShow: false,
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(),
     };
   },
   computed: {},
@@ -93,6 +146,18 @@ export default {
     handleBlur() {
       this.inshow = false;
     },
+    handleOk() {
+      this.message = this.message2;
+      this.textShow = false;
+    },
+    handleTime(value) {
+      console.log(value);
+      this.time = getFormat1(value);
+      this.timeShow = false;
+    },
+    handleCancel() {
+      this.timeShow = false;
+    },
   },
 };
 </script>
@@ -106,5 +171,24 @@ export default {
 }
 /deep/.bchigh > .van-grid-item__content {
   background-color: #ccc;
+}
+/deep/.van-row {
+  .van-col--12:first-child .van-button {
+    float: left;
+    background-color: #fff;
+    border: 0;
+  }
+  .van-col--12:last-child .van-button {
+    background-color: #fff;
+    border: 0;
+    float: right;
+  }
+}
+/deep/.van-col--10 .van-button {
+  width: 100%;
+}
+.textLabel {
+  font-size: 14px;
+  padding: 16px;
 }
 </style>
--- src/components/payCategory.vue
+++ src/components/payCategory.vue
@@ -33,16 +33,18 @@
         text="设置"
       />
     </van-grid>
-    <van-field
-      v-model="message"
-      rows="2"
-      autosize
-      label="备注"
-      type="textarea"
-      maxlength="50"
-      placeholder="备注"
-      show-word-limit
-    />
+    <van-row>
+      <van-col :span="12">
+        <van-button type="default" @click="textShow = true">{{
+          message ? message : "添加备注"
+        }}</van-button>
+      </van-col>
+      <van-col :span="12">
+        <van-button type="default" @click="timeShow = true">{{
+          time
+        }}</van-button>
+      </van-col>
+    </van-row>
     <van-number-keyboard
       :show="payshow"
       v-model="value"
@@ -51,11 +53,54 @@
       close-button-text="完成"
       @blur="show = false"
     />
+    <van-popup
+      v-model="textShow"
+      round
+      :style="{ width: '90%', padding: '5%' }"
+    >
+      <span class="textLabel">填写备注：</span>
+      <van-field
+        v-model="message2"
+        rows="2"
+        autosize
+        border
+        type="textarea"
+        maxlength="50"
+        placeholder="备注"
+        show-word-limit
+      />
+      <van-row>
+        <van-col span="10" offset="1">
+          <van-button round plain type="primary" @click="textShow = false"
+            >取消</van-button
+          >
+        </van-col>
+        <van-col span="10" offset="2"
+          ><van-button round type="primary" @click="handleOk">确认</van-button>
+        </van-col>
+      </van-row>
+    </van-popup>
+    <van-popup
+      v-model="timeShow"
+      round
+      :style="{ width: '90%', padding: '5%' }"
+    >
+      <van-datetime-picker
+        v-model="currentDate"
+        type="datetime"
+        title="选择完整时间"
+        :min-date="minDate"
+        :max-date="maxDate"
+        @confirm="handleTime"
+        @cancel="handleCancel"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
 import { payCategory } from "@/utils/Category.json";
+import { getFormat1 } from "@/utils/time.ts";
 export default {
   name: "payCategoryContainer",
   components: {},
@@ -63,10 +108,18 @@ export default {
   data() {
     return {
       payCategory: payCategory,
-      payshow: false,
+      payshow: true,
       value: "",
       message: "",
+      message2: "",
       selected: 0,
+      time: getFormat1(new Date()),
+      currentDate: new Date(),
+      textShow: false,
+      timeShow: false,
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(),
     };
   },
   computed: {},
@@ -89,6 +142,18 @@ export default {
     handleBlur() {
       this.payshow = false;
     },
+    handleOk() {
+      this.message = this.message2;
+      this.textShow = false;
+    },
+    handleTime(value) {
+      console.log(value);
+      this.time = getFormat1(value);
+      this.timeShow = false;
+    },
+    handleCancel() {
+      this.timeShow = false;
+    },
   },
 };
 </script>
@@ -102,5 +167,24 @@ export default {
 }
 /deep/.bchigh > .van-grid-item__content {
   background-color: #ccc;
+}
+/deep/.van-row {
+  .van-col--12:first-child .van-button {
+    float: left;
+    background-color: #fff;
+    border: 0;
+  }
+  .van-col--12:last-child .van-button {
+    background-color: #fff;
+    border: 0;
+    float: right;
+  }
+}
+/deep/.van-col--10 .van-button {
+  width: 100%;
+}
+.textLabel {
+  font-size: 14px;
+  padding: 16px;
 }
 </style>
